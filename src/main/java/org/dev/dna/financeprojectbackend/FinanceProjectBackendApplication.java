a6aece4d96b2518fad260494fc15dbf499d0d5e4
nyarko-dennis <ndenniskofi@gmail.com>
2024-09-03 21:46:16 +0000

    initial commit

---
+++ src/main/java/org/dev/dna/financeprojectbackend/FinanceProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package org.dev.dna.financeprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dev/dna/financeprojectbackend/FinanceProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev.dna.financeprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
